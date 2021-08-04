2fb778b98482df9da5bb55f42dfa532a54562836
mohsin <morteen1997@gmail.com>
2021-08-05 02:06:59 +0600

    nested child routs added

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
+import C1 from '../components/C1.vue'
+import C2 from '../components/C2.vue'
+import C3 from '../components/C3.vue'
+import Counter from '../components/Counter.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  
+    component: Home,
+    children: [
+      {
+        path: '/',
+        name: 'C1',
+        component: C1,
+        children: [
+          {
+            path: '/',
+            name: 'C2',
+            component: C2,
+            children: [
+              {
+                path: '/',
+                name: 'C3',
+                component: C3,
+                children: [
+                  {
+                    path: '/',
+                    name: 'Counter',
+                    component: Counter,
+                  },
+                ]
+              },
+            ]
+          }
+        ]
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
