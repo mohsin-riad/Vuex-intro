6f32fe6ae8f7cf448e95b7a09c1c57b6810f84cd
mohsin <morteen1997@gmail.com>
2021-08-06 02:20:36 +0600

    actions completed

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,15 +2,25 @@
   <div>
     <div  class="text-center mt-4 shadow-lg font-bold text-2xl rounded bg-gray-300 mx-auto max-w-md p-3">
         <h3 class="text-center"> {{ counter }}</h3>
-        <button @click="subFromCounter(value)" class="bg-red-400 px-4 rounded-3xl shadow-md">-</button>
-        <input class="rounded text-center bg-gray-200 mx-7" v-model="value" type="number" name="" id="">
-        <button @click="addToCounter(value)" class="bg-green-400 but px-4 rounded-3xl cursor-pointer shadow-md">+</button>
+    </div>
+    <div  class="text-center mt-4 shadow-lg font-bold text-2xl rounded bg-gray-400 mx-auto max-w-md p-3">
+        <button @click="subFromCounter(value)" class="bg-red-400 px-4 hover:bg-red-500 rounded-3xl shadow-md">-</button>
+        <input class="rounded text-center  bg-gray-200 mx-7" v-model="value" type="number" name="" id="">
+        <button @click="addToCounter(value)" class="bg-green-400 hover:bg-green-500 but px-4 rounded-3xl cursor-pointer shadow-md">+</button>
+        <h2></h2>
+    </div>
+    <div  class="text-center mt-4 shadow-lg font-bold text-2xl rounded bg-gray-200 mx-auto max-w-md p-3">
+        <h2 class="mb-2 text-base text-gray-600">Random Number <small class="text-xs text-gray-400">(with counter)</small></h2> 
+        <button @click="addRandomNo" class="hover:bg-green-600 grid-cols-3 mr-2 font-extrabold text-white  bg-green-500 text-lg py-2 px-4 hover:border-transparent rounded">+</button>
+        <button @click="subRandomNo" class="hover:bg-green-600 grid-cols-3 font-extrabold text-white  bg-green-500 text-lg py-2 px-4 mr-2 hover:border-transparent rounded">-</button>
+        <button @click="mulRandomNo" class="hover:bg-green-600 grid-cols-3 font-bold text-white  bg-green-500 text-lg py-2 px-4 mr-2 hover:border-transparent rounded">x</button>
+        <button @click="divRandomNo" class="hover:bg-green-600 grid-cols-3 font-extrabold text-white  bg-green-500 text-lg py-2 px-4 hover:border-transparent rounded">÷</button>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex' 
+import { mapState, mapMutations, mapActions } from 'vuex' 
 export default {
     data() {
         return {
@@ -21,8 +31,8 @@ export default {
         ...mapState(["counter"])
     },
     methods: {
-        ...mapMutations(["addToCounter"]),
-        ...mapMutations(["subFromCounter"]),
+        ...mapMutations(["addToCounter", "subFromCounter"]),
+        ...mapActions(["addRandomNo", "subRandomNo", "mulRandomNo", "divRandomNo"]),
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { createStore } from 'vuex'
 
 export default createStore({
@@ -11,9 +12,35 @@ export default createStore({
     subFromCounter(state, payload) {
       state.counter -= parseInt(payload);
     },
+    mulWithCounter(state, payload) {
+      state.counter *= parseInt(payload);
+    },
+    divFromCounter(state, payload) {
+      state.counter /= parseInt(payload);
+    },
 
   },
   actions: {
+    async addRandomNo(context) {
+      const baseURL = "https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new";
+      const response = await axios.get(baseURL);
+      context.commit("addToCounter", response.data);
+    },
+    async subRandomNo(context) {
+      const baseURL = "https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new";
+      const response = await axios.get(baseURL);
+      context.commit("subFromCounter", response.data);
+    },
+    async mulRandomNo(context) {
+      const baseURL = "https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new";
+      const response = await axios.get(baseURL);
+      context.commit("mulWithCounter", response.data);
+    },
+    async divRandomNo(context) {
+      const baseURL = "https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new";
+      const response = await axios.get(baseURL);
+      context.commit("divFromCounter", response.data);
+    },
   },
   modules: {
   }
